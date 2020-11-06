da7670a9ba132b8c9fdc4fbdd78ecdd5908c3896
DroidDumps <rokibhasansagar2014@outlook.com>
2020-11-06 15:40:20 +0000

    Add full_gnsz6753_66_n-user 7.0 NRD90M 1493378589 release-keys
    Signed-off-by: DroidDumps <rokibhasansagar2014@outlook.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+## full_gnsz6753_66_n-user 7.0 NRD90M 1493378589 release-keys
+- Manufacturer: condor
+- Platform: mt6753
+- Codename: PGN611
+- Brand: condor
+- Flavor: full_gnsz6753_66_n-user
+- Release: 7.0
+- Id: NRD90M
+- Incremental: 1493378589
+- Tags: release-keys
+- Fingerprint: condor/PGN611/PGN611:7.0/NRD90M/1493378589:user/release-keys
+- Branch: full_gnsz6753_66_n-user-7.0-NRD90M-1493378589-release-keys
+- Repo: condor_pgn611_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat system/priv-app/GmsCore/oat/arm64/GmsCore.odex.* 2>/dev/null >> system/priv-app/GmsCore/oat/arm64/GmsCore.odex
+rm -f system/priv-app/GmsCore/oat/arm64/GmsCore.odex.* 2>/dev/null
+cat system/priv-app/GmsCore/oat/arm/GmsCore.odex.* 2>/dev/null >> system/priv-app/GmsCore/oat/arm/GmsCore.odex
+rm -f system/priv-app/GmsCore/oat/arm/GmsCore.odex.* 2>/dev/null
+cat system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/app/Chrome/Chrome.apk.* 2>/dev/null >> system/app/Chrome/Chrome.apk
+rm -f system/app/Chrome/Chrome.apk.* 2>/dev/null
